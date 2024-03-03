d4a60103a2cb3da54bbde76bf97c64b4e6f8e61f
GersonFeDutra <gerson.creative.des@gmail.com>
2024-03-03 18:23:55 -0300

    Setup Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <>
-      <Home />
-      {/* <Profile /> */}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="*" element={<Home />} /> {/* TODO -> Page Not Found */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,36 +1,40 @@
 import Feed from '../../components/Feed';
 import Layout from '../../components/Layout';
+import Login from '../Login';
 import { LogoTypography } from './styles';
 
 const Home = () => {
   return (
-    <Layout hasBackPage={false}>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'start',
-          alignItems: 'center',
-          // height: '31px',
-          // padding: '25px 16px',
-        }}
-      >
+    <>
+      <Layout hasBackPage={false}>
         <div
           style={{
-            paddingLeft: '16px',
-            color: '#ababab',
-            marginTop: '30px',
             display: 'flex',
             justifyContent: 'start',
             alignItems: 'center',
-            fontSize: '30px',
-            fontWeight: 'bolder',
+            // height: '31px',
+            // padding: '25px 16px',
           }}
         >
-          Página <LogoTypography>Y</LogoTypography> Inicial
+          <div
+            style={{
+              paddingLeft: '16px',
+              color: '#ababab',
+              marginTop: '30px',
+              display: 'flex',
+              justifyContent: 'start',
+              alignItems: 'center',
+              fontSize: '30px',
+              fontWeight: 'bolder',
+            }}
+          >
+            Página <LogoTypography>Y</LogoTypography> Inicial
+          </div>
         </div>
-      </div>
-      <Feed />
-    </Layout>
+        <Feed />
+      </Layout>
+      <Login />
+    </>
   );
 };
 
