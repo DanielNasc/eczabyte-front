264df36f9e7f8de89e0cae20f33d3c0916fb049e
Marcos Chalet <chaletmarcos@gmail.com>
2024-03-03 15:34:57 -0300

    Adiciona rotas no projeto

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Settings from './pages/Settings';
+import NotFound from './pages/404Voyage';
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/profile', element: <Profile /> },
+  { path: '/settings', element: <Settings /> },
+  { path: '/login', element: <div>Login</div> },
+  { path: '/signup', element: <div>Sign Up</div> },
+  { path: '*', element: <NotFound /> },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
